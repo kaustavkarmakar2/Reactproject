b073311ea70acfbe7d3989db261582e0e05af6c3
Kaustav Karmakar <kaustavkarmakar@Kaustavs-MacBook-Air.local>
2020-03-17 13:41:10 +0530

    Updates

---
+++ precache-manifest.f7a1ecd0ac903334e730f5ebdb789df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d97914c318cbde8cbb804df3e65ed3",
+    "url": "/Reactproject/my-app/index.html"
+  },
+  {
+    "revision": "81a423192f31ffb43c26",
+    "url": "/Reactproject/my-app/static/css/main.cf130a84.chunk.css"
+  },
+  {
+    "revision": "38548f4ada0bb37beb6e",
+    "url": "/Reactproject/my-app/static/js/2.ca113ce4.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Reactproject/my-app/static/js/2.ca113ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a423192f31ffb43c26",
+    "url": "/Reactproject/my-app/static/js/main.933f5025.chunk.js"
+  },
+  {
+    "revision": "06d293ae849818a66394",
+    "url": "/Reactproject/my-app/static/js/runtime-main.9cfcabc2.js"
+  }
+]);
